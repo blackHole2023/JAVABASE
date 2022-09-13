90c99a30f42c292724029b53c51cd6cd59cc5aba
q1621572449 <42181651+q1621572449@users.noreply.github.com>
2022-09-13 10:58:58 +0800

    first commit

---
+++ lambda/src/LambdaExpressions.java
@@ -0,0 +1,43 @@
+interface Description{
+    String brief();
+}
+
+interface Body{
+    String detailed(String head);
+}
+
+interface Multi {
+    String twoArg(String head, Double d);
+}
+
+
+
+public class LambdaExpressions {
+//    static Description description=h->h+"No P";
+//    static Body body;
+//    static Multi multi;
+    static Body bod = h->h+"No Parens";
+    static Body bod2 = h->h+"More details";
+    static Description description = ()->"short info";
+    static Multi multi = (h,n)->h+n;
+    static Description morelines = ()->{
+        System.out.println("moreLines()");
+        return "from moreLines()";
+    };
+
+    public static void main(String[] args) {
+        Body body = h->h+"No Parens";//相当于一个函数，与下面是等价的，重写了body中的方法。函数式编程。
+        Body body1 = new Body() {
+            @Override
+            public String detailed(String head) {
+                return head+"No Parens";
+            }
+        };
+
+
+
+        System.out.println(bod.detailed("oh!"));
+    }
+
+
+}
