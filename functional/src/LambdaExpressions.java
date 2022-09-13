5f6381ed591f60ba89d70b03483afa49d7b64258
q1621572449 <42181651+q1621572449@users.noreply.github.com>
2022-09-13 11:08:11 +0800

    1

--- functional/src/LambdaExpressions.java
+++ functional/src/LambdaExpressions.java
@@ -18,7 +18,7 @@ public class LambdaExpressions {
 //    static Multi multi;
     static Body bod = h->h+"No Parens";
     static Body bod2 = h->h+"More details";
-    static Description description = ()->"short info";
+    static Description description = ()->"short info";//参数要对应，原方法是无参的，这里也应该是无参。还有，返回值是不是也应该对应呢？
     static Multi multi = (h,n)->h+n;
     static Description morelines = ()->{
         System.out.println("moreLines()");
@@ -27,15 +27,13 @@ public class LambdaExpressions {
 
     public static void main(String[] args) {
         Body body = h->h+"No Parens";//相当于一个函数，与下面是等价的，重写了body中的方法。函数式编程。
+
         Body body1 = new Body() {
             @Override
             public String detailed(String head) {
                 return head+"No Parens";
             }
         };
-
-
-
         System.out.println(bod.detailed("oh!"));
     }
 
