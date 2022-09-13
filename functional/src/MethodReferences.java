d77ff21b639eb1968aa20fd284e4d48077da2961
q1621572449 <42181651+q1621572449@users.noreply.github.com>
2022-09-13 22:33:12 +0800

    MethodReferences

---
+++ functional/src/MethodReferences.java
@@ -0,0 +1,15 @@
+interface Callabel{
+    void call(String s);
+}
+
+class Describe{
+    void show(String msg) {
+        System.out.println(msg);
+    }
+}
+
+public class MethodReferences {
+
+
+
+}
