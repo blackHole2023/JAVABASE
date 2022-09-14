8f0e7fb4b9828afc0fad9d12cad92575f6c1b83e
q1621572449 <42181651+q1621572449@users.noreply.github.com>
2022-09-14 10:24:37 +0800

    函数式编程

---
+++ functional/src/MethodConversion.java
@@ -0,0 +1,23 @@
+import java.util.function.*;
+
+class In1 {}
+class In2 {}
+
+public class MethodConversion {
+    static void accept(In1 i1, In2 i2) {
+        System.out.println("accept()");
+    }
+    static void someOtherName(In1 i1, In2 i2) {
+        System.out.println("someOtherName()");
+    }
+    public static void main(String[] args) {
+        BiConsumer<In1,In2> bic;
+
+        bic = MethodConversion::accept;
+        bic.accept(new In1(), new In2());
+
+        bic = MethodConversion::someOtherName;//与方法名无关，只跟返回名和参数有关。
+        // bic.someOtherName(new In1(), new In2()); // Nope
+        bic.accept(new In1(), new In2());
+    }
+}
