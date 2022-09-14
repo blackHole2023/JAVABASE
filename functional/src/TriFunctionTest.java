163067c96e50ccdf0533ff8d5a462dba00a177a7
q1621572449 <42181651+q1621572449@users.noreply.github.com>
2022-09-14 14:16:46 +0800

    函数式编程

--- functional/src/TriFunctionTest.java
+++ functional/src/TriFunctionTest.java
@@ -1,2 +1,10 @@
 public class TriFunctionTest {
+    static int f(int i, long l, double d) { return 99; }
+    public static void main(String[] args) {
+        TriFunction<Integer, Long, Double, Integer> tf =
+                TriFunctionTest::f;
+//        tf = (i, l, d) -> 12;
+        Integer apply = tf.apply(1, 2l, 3d);
+        System.out.println(apply);
+    }
 }
