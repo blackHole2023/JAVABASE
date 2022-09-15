7ff285f86f6ec5d7f070169e70f5ee2d2271d360
q1621572449 <42181651+q1621572449@users.noreply.github.com>
2022-09-15 19:05:08 +0800

    流式编程

---
+++ Streams/src/ImperativeRandoms.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class ImperativeRandoms {
+    public static void main(String[] args) {
+        Random rand = new Random(47);
+        SortedSet<Integer> rints = new TreeSet<>();//treeSet,方便比较的容器
+        while(rints.size() < 7) {
+            int r = rand.nextInt(20);
+            if(r < 5) continue;
+            rints.add(r);
+        }
+        System.out.println(rints);
+    }
+}
