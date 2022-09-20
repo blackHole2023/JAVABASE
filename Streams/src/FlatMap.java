5f591139fd16cbc545d076dee52fa019d13bb153
q1621572449 <42181651+q1621572449@users.noreply.github.com>
2022-09-20 19:55:42 +0800

    流编程

---
+++ Streams/src/FlatMap.java
@@ -0,0 +1,10 @@
+import java.util.stream.*;
+
+public class FlatMap {
+    public static void main(String[] args) {
+        Stream.of(1, 2, 3,4,5)
+                .flatMap(
+                        i -> Stream.of("Gonzo", "Fozzie", "Beaker"))
+                .forEach(System.out::println);
+    }
+}
