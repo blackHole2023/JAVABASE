e80a71d8cc206fff719655a4a7d895a29f0cb971
q1621572449 <42181651+q1621572449@users.noreply.github.com>
2022-09-20 10:26:09 +0800

    流编程

--- Streams/src/Prime.java
+++ Streams/src/Prime.java
@@ -3,11 +3,11 @@ import static java.util.stream.LongStream.*;
 
 public class Prime {
     public static boolean isPrime(long n) {
-        return rangeClosed(2, (long)Math.sqrt(n))
+        return rangeClosed(2, (long)Math.sqrt(n))//这里的2想到那个鱼i吧。。。迭代的参数，一直到输入为n。这写法实在是太高级了。
                 .noneMatch(i -> n % i == 0);
     }
     public LongStream numbers() {
-        return iterate(2, i -> i + 1)//迭代器，从2开始迭代，递增，filter筛选出true的元素。
+        return iterate(1, i -> i + 1)//迭代器，从2开始迭代，递增，filter筛选出true的元素。
                 .filter(Prime::isPrime);
     }
     public static void main(String[] args) {
