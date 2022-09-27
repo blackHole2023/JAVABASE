610bdb9ae31e3fd6e458072027c5e478fbb14185
q1621572449 <42181651+q1621572449@users.noreply.github.com>
2022-09-27 15:18:28 +0800

    异常

---
+++ Exceptions/src/exceptions/TryAnything.java
@@ -0,0 +1,12 @@
+package exceptions;
+
+class Anything {}
+
+public class TryAnything {
+    public static void main(String[] args) {
+//        try(
+//                Anything a = new Anything()//会报错。没有继承autoclose接口
+//        ) {
+//        }
+    }
+}
