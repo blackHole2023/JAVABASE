68816591079d0fc9d7e79406386dd4ba56fb94f7
q1621572449 <42181651+q1621572449@users.noreply.github.com>
2022-09-29 10:06:47 +0800

    代码校验

---
+++ validating/src/main/java/validating/CircularQueueException.java
@@ -0,0 +1,9 @@
+package validating;
+
+
+public class
+CircularQueueException extends RuntimeException {
+    public CircularQueueException(String why) {
+        super(why);
+    }
+}
---
+++ validating/src/main/java/validating/LoaderAssertions.java
@@ -0,0 +1,15 @@
+package validating;
+
+public class LoaderAssertions {
+    public static void main(String[] args) {
+        ClassLoader.getSystemClassLoader()
+                .setDefaultAssertionStatus(true);
+        new Loaded().go();
+    }
+}
+
+class Loaded {
+    public void go() {
+        assert false: "Loaded.go()";
+    }
+}
