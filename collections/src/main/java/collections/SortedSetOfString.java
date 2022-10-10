0ca305eb5d7d6008be841ca6cdae4dd837c6b627
q1621572449 <42181651+q1621572449@users.noreply.github.com>
2022-10-10 15:59:43 +0800

    collections

---
+++ collections/src/main/java/collections/SortedSetOfString.java
@@ -0,0 +1,23 @@
+package collections;
+
+import java.util.*;
+
+public class SortedSetOfString {
+    public static void main(String[] args) {
+        Set<String> colors = new TreeSet<>();
+        for(int i = 0; i < 100; i++) {
+            colors.add("Yellow");
+            colors.add("Blue");
+            colors.add("Red");
+            colors.add("Red");
+            colors.add("Orange");
+            colors.add("Yellow");
+            colors.add("Blue");
+            colors.add("Purple");
+        }
+        System.out.println(colors);
+    }
+}
+/* Output:
+[Blue, Orange, Purple, Red, Yellow]
+*/
---
+++ collections/src/main/java/collections/UniqueWords.java
@@ -0,0 +1,30 @@
+package collections;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class UniqueWords {
+    public static void
+    main(String[] args) throws Exception {
+        List<String> lines = Files.readAllLines(
+                Paths.get("collections\\src\\main\\java\\collections\\SetOperations.java"));
+        Set<String> words = new TreeSet<>();
+        for(String line : lines)
+            for(String word : line.split("\\W+"))
+                if(word.trim().length() > 0)
+                    words.add(word);
+        System.out.println(words);
+    }
+}
+/* Output:
+[A, B, C, Collections, D, E, F, G, H, HashSet, I, J, K,
+L, M, N, Output, Set, SetOperations, String, System, X,
+Y, Z, add, addAll, added, args, class, collections,
+contains, containsAll, false, from, import, in, java,
+main, new, out, println, public, remove, removeAll,
+removed, set1, set2, split, static, to, true, util,
+void]
+*/
