73cd2722381ef1c0b208910a492e517ed24a9909
q1621572449 <42181651+q1621572449@users.noreply.github.com>
2022-10-10 18:11:16 +0800

    collections

---
+++ collections/src/main/java/collections/QueueDemo.java
@@ -0,0 +1,27 @@
+package collections;
+
+import java.util.*;
+
+public class QueueDemo {
+    public static void printQ(Queue queue) {
+        while(queue.peek() != null)
+            System.out.print(queue.remove() + " ");
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        Queue<Integer> queue = new LinkedList<>();//多继承
+        Random rand = new Random(47);
+        for(int i = 0; i < 10; i++)
+            queue.offer(rand.nextInt(i + 10));
+        printQ(queue);
+        Queue<Character> qc = new LinkedList<>();
+        for(char c : "Brontosaurus".toCharArray())
+            qc.offer(c);
+        printQ(qc);
+    }
+}
+/* Output:
+8 1 1 1 5 14 3 1 0 1
+B r o n t o s a u r u s
+*/
+
