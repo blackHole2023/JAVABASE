b67d2c83ed9849f060eebb7b5fcaa16287ee8533
q1621572449 <42181651+q1621572449@users.noreply.github.com>
2022-10-10 17:14:46 +0800

    collections

---
+++ collections/src/main/java/collections/PetMap.java
@@ -0,0 +1,26 @@
+package collections;
+
+import reflection.pets.*;
+
+import java.util.*;
+
+public class PetMap {
+    public static void main(String[] args) {
+        Map<String, Pet> petMap = new HashMap<>();
+        petMap.put("My Cat", new Cat("Molly"));
+        petMap.put("My Dog", new Dog("Ginger"));
+        petMap.put("My Hamster", new Hamster("Bosco"));
+        System.out.println(petMap);
+        Pet dog = petMap.get("My Dog");
+        System.out.println(dog);
+        System.out.println(petMap.containsKey("My Dog"));
+        System.out.println(petMap.containsValue(dog));
+    }
+}
+/* Output:
+{My Dog=Dog Ginger, My Cat=Cat Molly, My
+Hamster=Hamster Bosco}
+Dog Ginger
+true
+true
+*/
