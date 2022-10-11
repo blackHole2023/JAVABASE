affd7b0e48f720b88986f24bf5b2334c244fa938
q1621572449 <42181651+q1621572449@users.noreply.github.com>
2022-10-11 22:51:42 +0800

    字符串

--- strings/src/main/java/strings/InfiniteRecursion.java
+++ strings/src/main/java/strings/InfiniteRecursion.java
@@ -5,11 +5,14 @@ import java.util.stream.Stream;
 public class InfiniteRecursion {
     @Override public String toString() {
         return
-                " InfiniteRecursion address: " + this + "\n";//发生递归，报错。+会发生类型转发，调用tostring方法。
+//                " InfiniteRecursion address: " + this + "\n";//发生递归，报错。+会发生类型转发，调用tostring方法。
+//        //发生递归，报错。+会发生类型转发，调用tostring方法。
+        " InfiniteRecursion address: " + super.toString() + "\n";//正确的
     }
     public static void main(String[] args) {
         Stream.generate(InfiniteRecursion::new)
                 .limit(10)
                 .forEach(System.out::println);
+        String s = "str";
     }
 }
