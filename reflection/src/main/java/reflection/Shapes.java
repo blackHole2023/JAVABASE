7f38cae74acaf0de4aa8deaf186c47745bda5eae
q1621572449 <42181651+q1621572449@users.noreply.github.com>
2022-10-13 11:18:07 +0800

    反射

--- reflection/src/main/java/reflection/Shapes.java
+++ reflection/src/main/java/reflection/Shapes.java
@@ -1,4 +1,41 @@
 package reflection;
 
-public class Shapes {
+import java.util.stream.*;
+
+abstract class Shape {
+    void draw() {
+        System.out.println(this + ".draw()");
+    }
+    @Override public abstract String toString();
 }
+
+class Circle extends Shape {
+    @Override public String toString() {
+        return "Circle";
+    }
+}
+
+class Square extends Shape {
+    @Override public String toString() {
+        return "Square";
+    }
+}
+
+class Triangle extends Shape {
+    @Override public String toString() {
+        return "Triangle";
+    }
+}
+
+public class Shapes {
+    public static void main(String[] args) {
+        Stream.of(
+                new Circle(), new Square(), new Triangle())
+                .forEach(Shape::draw);
+    }
+}
+/* Output:
+Circle.draw()
+Square.draw()
+Triangle.draw()
+*/
