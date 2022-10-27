656e229c7702e0b79b0e97a3a52a351a841a1746
HuJieyu <42181651+q1621572449@users.noreply.github.com>
2022-10-27 16:38:25 +0800

    注解

---
+++ annotations/src/main/java/annotations/Testable.java
@@ -0,0 +1,16 @@
+// annotations/Testable.java
+// (c)2021 MindView LLC: see Copyright.txt
+// We make no guarantees that this code is fit for any purpose.
+// Visit http://OnJava8.com for more book information.
+package annotations;
+
+
+import org.junit.jupiter.api.Test;
+
+public class Testable {
+  public void execute() {
+    System.out.println("Executing..");
+  }
+  @Test
+  void testExecute() { execute(); }
+}
